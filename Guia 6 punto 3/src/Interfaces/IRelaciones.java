4f0ee5a0f42e76a08bfa9ad8229200da4d5594f6
Matias Antonelli <matiasantonelli6874@gmail.com>
2024-05-02 10:48:49 -0300

    Delete Guia 6 punto 3 directory

--- Guia 6 punto 3/src/Interfaces/IRelaciones.java
+++
@@ -1,9 +0,0 @@
-package Interfaces;
-
-public interface IRelaciones {
-
-    boolean esMayor(Object b); // Devuelve verdadero si un mumero es mayor que b
-    boolean esMenor(Object b); // Devuelve verdadero si un numero es menor que b
-    boolean esIgual(Object b);  // Devuelve verdadero si a es igual que b
-
-}
